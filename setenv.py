eea3ab90adfa4b8dc9441e8035c198f7e8a62af0
WelkinSoftware <clewis222@me.com>
2014-06-17 12:16:44 -0700

    Test setenv change

--- setenv.py
+++ setenv.py
@@ -4,4 +4,3 @@ from multiquest.models import *
 from multiquest.views import *
 from multiquest.utilities import *
 from multiquest.utilities_db import *
-from django.contrib.auth.models import Group, User, Permission
